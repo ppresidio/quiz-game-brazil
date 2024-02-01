67a38e85de28b39d61ad690dc3b537ec224c2169
ppresidio <158526499+ppresidio@users.noreply.github.com>
2024-02-01 16:09:04 -0700

    Add files via upload

---
+++ quiz_game.py
@@ -0,0 +1,44 @@
+"""
+Name: quiz_game.py
+ Author: Pedro Paulo Presidio
+ Created: 1/22/24
+ Purpose: A quiz game about Brazil
+"""
+#--------------CONSTANTS------------------
+DASHES = 50
+
+#---------------GAME TITEL---------------
+print("-" * DASHES)
+print("The Brazilian quiz game :0")
+print("-" * DASHES)
+#------------------INPUT-----------------
+#TODO:Print the difficult levels
+print("For this game we will have 3 difficulties:")
+print("Level 1 - Just easy questions")
+print("Level 2 - General knowledge about Brazil")
+print("Level 3 - This one is hard even for Brazilians")
+
+
+#TODO:Recive the level of difficult that the user wants
+difficult_level = int(input("Select which of the levels you want to play 1, 2 or 3: "))
+
+#TODO:Create the variables to save the score and the amount of questions in each category.
+
+
+#TODO:Create the conditions to check which level the user choose
+
+
+#TODO: Create a structure that do the first level, while there is still questions to answer.
+
+
+#TODO: Create a structure that do the second level, while there is still questions to answer.
+
+
+#TODO: Create a structure that do the third level, while there is still questions to answer.
+
+
+#TODO: Print the results, and ask if he wants to play another game
+
+
+#TODO: If he decide not to play, print a message with the score again, and tell about the user performace
+
